cf8e3d8b46d23fb5d7b888294e2f313fcbb6e0df
XGalilei <ianbush1@gmail.com>
2021-09-30 13:51:07 -0500

    POST method complete, stubs for remaining two queries added in

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  const queryText = `SELECT * FROM "favorites";`;
+  const queryText = `SELECT * FROM "favorites" ORDER BY "title" ASC;`;
   pool.query(queryText).then(
     result => res.send(result.rows))
   .catch(error => {
@@ -16,18 +16,41 @@ router.get('/', (req, res) => {
 
 // add a new favorite
 router.post('/', (req, res) => {
-  res.sendStatus(200);
+  const queryText = `INSERT INTO "favorites" VALUES ("title", "link")
+  VALUES ($1 $2);`
+  const queryValues = [req.body.title, req.body.link];
+  pool.query(queryText, queryValues).then(() => {
+    res.sendStatus(201);
+  }).catch(error => {
+      console.log('Whoopsies...', error);
+      res.sendStatus(500);
+    }
+  );
 });
 
 // update given favorite with a category id
 router.put('/:favId', (req, res) => {
   // req.body should contain a category_id to add to this favorite image
-  res.sendStatus(200);
+  
+  const queryText = `UPDATE "favorites SET `;
+  pool.query(queryText).then(() => {
+    res.sendStatus(200);
+  }).catch(error => {
+    console.log('Whoopsies...', error);
+    res.sendStatus(500);
+  })
 });
 
 // delete a favorite
-router.delete('/', (req, res) => {
-  res.sendStatus(200);
+router.delete('/:favId', (req, res) => {
+  const queryText = '';
+  pool.query(queryText).then(() => {
+    res.sendStatus(200);
+  }).catch(error => {
+    console.log('Whoopsy-daisies...', error);
+    res.sendStatus(500);
+  })
+  
 });
 
 module.exports = router;
