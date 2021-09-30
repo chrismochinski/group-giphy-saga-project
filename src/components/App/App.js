4e0f79e946b43368f4626eae7885ed6bcd8e4520
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-30 14:03:33 -0500

    Imported SearchPage to App.jsx

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchPage from '../SearchPage/SearchPage.jsx'
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <SearchPage />
     </div>
   );
 }
