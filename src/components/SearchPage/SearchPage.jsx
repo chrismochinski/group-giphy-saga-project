d1c192e559a45e7470c584ce3ae271ccae3609b4
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-30 13:32:55 -0500

    added imports

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,4 +1,13 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+
 function SearchPage(){
+
+    const [newSearch, setNewSearch] = useState('');
+
+    
+
     return(
         <>
         </>
