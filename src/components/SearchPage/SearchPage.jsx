e6d176cd2ff4141adf96414affe383fd53d7ae2a
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-09-30 13:36:08 -0500

    Created form and added addNewSearch dispatch function

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -6,10 +6,25 @@ function SearchPage(){
 
     const [newSearch, setNewSearch] = useState('');
 
-    
+    const addNewSearch = (event) => {
+        event.preventDefault();
+        console.log('New search: ', newSearch);
+        dispatchEvent({ type: 'FETCH_SEARCH', payload: newSearch })
+    }
 
     return(
         <>
+        <form onSubmit={addNewSearch}>
+            <input 
+                required
+                placeholder='E.G.: Cats'
+                value={newSearch}
+                onChange={(event) => setNewSearch(event.target.value)}
+            /><br />
+            <button type='submit'>Search!</button>
+        </form>
+        <h1>Here are your Gifs:</h1>
+        <img src={newSearch}></img>
         </>
     )
 }
