f337d129cd5a133a45d2a007ab03f1444908b1a0
anwar <anwarhussein2353@gmail.com>
2021-10-01 09:17:13 -0500

    Added Favorite item component

--- src/components/FavoriteItem/FavoriteItem.jsx
+++ src/components/FavoriteItem/FavoriteItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import {useState} from 'react';
+import useDispatch from 'react-redux';
 
 function FavoriteItem() {
+
+   const dispatch = useDispatch();
+
+   const addFavorite = () => [
+       dispatch ({type: 'ADD_FAVORITE', payload:fav})
+   ]
+    
     return (
         <div>
-            
+            <li></li>
+   
         </div>
     )
 }
--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
+
 function FavoriteList() {
+   
     return (
         <div>
             
